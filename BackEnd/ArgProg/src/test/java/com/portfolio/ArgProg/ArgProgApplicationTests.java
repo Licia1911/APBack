2b7fd0a569d89a2780fa25904064a4317d529cbb
Licia <licia.carreras@gmail.com>
2022-10-06 10:19:38 -0300

    Se inicia Back end

---
+++ BackEnd/ArgProg/src/test/java/com/portfolio/ArgProg/ArgProgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.ArgProg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgProgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
