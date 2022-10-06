928d5e8cad8a5387ea966ae32e726dfb4ebef84e
Licia <licia.carreras@gmail.com>
2022-10-06 07:43:33 -0300

    Componentes 3,4,5
    Acerca-educación-experiencia/ finalizados y responsivos

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercaComponent } from './components/acerca/acerca.component';
+import { EducacionComponent } from './components/acerca/educacion/educacion.component';
+import { ExperienciaComponent } from './components/acerca/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaComponent,
+    EducacionComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule,
